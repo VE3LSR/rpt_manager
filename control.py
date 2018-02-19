b3b61a344e65cfe7913058e9f3075385baa99a9d
CoolAcid <coolacid@gmail.com>
2018-02-19 17:10:51 -0500

    Probably not the correct place for this

--- control.py
+++ control.py
@@ -8,6 +8,7 @@ import tornado.ioloop
 import tornado.web
 
 from rptmgr.allstar import allstar
+from rptmgr.asterisk import asterisk
 
 class MainHandler(tornado.web.RequestHandler):
 
@@ -30,6 +31,13 @@ if __name__ == "__main__":
             sys.exit(0)
 
     print (config)
+
+    for controller in config['asterisk']:
+        controller, values = list(controller.items())[0]
+        if 'enabled' in values and values['enabled']:
+            print("Starting controller: %s" % controller)
+            print("Values: %s" % values)
+            testA = asterisk(values['address'], values['port'], values['user'], values['password'])
 
     app = make_app()
     app.listen(config['listen_port'])
